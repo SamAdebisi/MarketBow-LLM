b951beaa8b7d3caae46d44e88acd60b95cd26b69
Azeez Adebisi <samadebisi12@gmail.com>
2025-07-23 01:38:39 +0300

    Update filter.py

--- dataset_creation/filter.py
+++ dataset_creation/filter.py
@@ -7,3 +7,6 @@ def get_basic_stats(text):
     line_lengths = [len(line) for line in text.split("\n")]
     max_line_length = max(line_lengths)
     mean_line_length = sum(line_lengths) / len(line_lengths)
+    alphanum_count = sum(char.isalpha() or char.isdigit() for char in text)
+    alphanum_ratio = alphanum_count / len(text)
+    return max_line_length, mean_line_length, alphanum_ratio 
