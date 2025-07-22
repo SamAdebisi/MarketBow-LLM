b6e5cd66ad9c1dfa98a64d5c1b85c66548b196d0
Azeez Adebisi <samadebisi12@gmail.com>
2025-07-22 14:58:07 +0300

    Update pipeline.py

--- dataset_creation/pipeline.py
+++ dataset_creation/pipeline.py
@@ -7,3 +7,16 @@ from datatrove.pipeline.dedup.minhash import (
     MinhashDedupCluster, 
     MinhashDedupFilter, 
 )
+from datatrove.pipeline.tokens import TokensCounter 
+from datatrove.pipeline.readers import JsonlReader 
+from datatrove.pipeline.writers.jsonl import JsonlWriter 
+from reader import PersonalCopilotDatasetReader 
+from filter import BasicodeFilter 
+
+MIRROR_DIRECTORY = "MarketBow-LLM" 
+TOTAL_TASKS = 16 
+
+# you can also change ngrams or the number of buckets and their size here 
+minhash_config = MinhashConfig(
+    use_64bit_hashes=True, 
+) # better precision -> fewer false positives (collisions)
