3aed14f3a0e2dcca02ddeec1446124e0503080b4
Azeez Adebisi <samadebisi12@gmail.com>
2025-07-23 03:13:51 +0300

    Update clone_marketbow_llm.py

--- dataset_creation/clone_marketbow_llm.py
+++ dataset_creation/clone_marketbow_llm.py
@@ -12,3 +12,13 @@ def get_repos(username, access_token=None, include_fork=False):
     """ """
     g = Github(access_token)
     user = g.get_user(username) 
+    
+    results = [] 
+    for repo in user.get_repos():
+        if repo.fork is False: 
+            results.append((repo.name, repo.stargazers_count))
+        else:
+            if include_fork is True: 
+                results.append((repo.name, repo.stargazers_count))
+    print(results)
+    return results 
