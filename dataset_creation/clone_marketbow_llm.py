fc7574947f132e11c613f2c42120bb34dbbe0ce7
Azeez Adebisi <samadebisi12@gmail.com>
2025-07-23 03:25:42 +0300

    Update clone_marketbow_llm.py

--- dataset_creation/clone_marketbow_llm.py
+++ dataset_creation/clone_marketbow_llm.py
@@ -26,3 +26,12 @@ def get_repos(username, access_token=None, include_fork=False):
 
 def sort_repos_by_stars(repos):
     return sorted(repos, key=lambda x: x[1], reverse=True)
+
+
+def mirror_repository(repository):
+    """Locally clones a repository."""
+    repository_url = f"https://github.com/{ORG}/{repository}.git" 
+    repository_path = os.path.join(MIRROR_DIRECTORY, repository)
+    
+    # Clone the repository 
+    subprocess.run(["git", "clone", repository_url, repository_path])
