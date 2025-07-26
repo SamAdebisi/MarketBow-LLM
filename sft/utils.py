4bb20d66bb047b5843e479f829e2facf13394d1b
Azeez Adebisi <samadebisi12@gmail.com>
2025-07-26 18:12:36 +0300

    Update utils.py

---
+++ sft/utils.py
@@ -0,0 +1,18 @@
+from enum import Enum 
+import os 
+import torch 
+from datasets import DatasetDict, concatenate_datasets, load_dataset, load_from_disk 
+from datasets.builder import DatasetGenerationError 
+from tqdm import tqdm 
+from peft import Loraconfig 
+from peft.tuners.lora import LoraLayer 
+from transformers import (
+    AutoModelForCausalLM, 
+    AutoTokenizer, 
+    BitsAndBytesConfig, 
+)
+
+DEFAULT_CHATML_CHAT_TEMPLATE = "{% for message in messages %}\n{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% if loop.last and add_generation_prompt %}{{'<|im_start|>assistant\n' }}{% endif %}{% endfor %}"
+DEFAULT_ZEPHYR_CHAT_TEMPLATE = "{% for message in messages %}\n{% if message['role'] == 'user' %}\n{{ '<|user|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'system' %}\n{{ '<|system|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'assistant' %}\n{{ '<|assistant|>\n'  + message['content'] + eos_token }}\n{% endif %}\n{% if loop.last and add_generation_prompt %}\n{{ '<|assistant|>' }}\n{% endif %}\n{% endfor %}"
+
+
