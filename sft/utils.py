6adae239b473ba26e233259afc2a5ecce4061132
Azeez Adebisi <samadebisi12@gmail.com>
2025-07-26 18:17:54 +0300

    Update utils.py

--- sft/utils.py
+++ sft/utils.py
@@ -16,3 +16,10 @@ DEFAULT_CHATML_CHAT_TEMPLATE = "{% for message in messages %}\n{{'<|im_start|>' 
 DEFAULT_ZEPHYR_CHAT_TEMPLATE = "{% for message in messages %}\n{% if message['role'] == 'user' %}\n{{ '<|user|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'system' %}\n{{ '<|system|>\n' + message['content'] + eos_token }}\n{% elif message['role'] == 'assistant' %}\n{{ '<|assistant|>\n'  + message['content'] + eos_token }}\n{% endif %}\n{% if loop.last and add_generation_prompt %}\n{{ '<|assistant|>' }}\n{% endif %}\n{% endfor %}"
 
 
+class ZephyrSpecialTokens(str, Enum):
+    user = "<|user|>"
+    assistant = "<|assistant|>"
+    system = "<|system|>"
+    eos_token = "</s>"
+    bos_token = "<s>"
+    pad_token = "<pad>"
